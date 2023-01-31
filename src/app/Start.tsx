fe39077fffbe6f41555dfe1ae3238ec5bb3b888d
damianamodeo <damianamodeotheocratic@gmail.com>
2023-02-01 07:43:26 +1100

    scriptBackup

--- src/app/Start.tsx
+++ src/app/Start.tsx
@@ -1,11 +1,9 @@
-import ButtonsPage from '@PAGES/buttons/buttons';
-import IconsPage from '@PAGES/icons/IconsPage';
-import InputsPage from '@PAGES/inputs/InputsPage';
-import Navbar from '@UICOMPONENTS/containers/Navbar';
-import Screen from '@UICOMPONENTS/containers/Screen';
+import ButtonsPage from '../pages/buttons/buttons';
+import Navbar from '../components/containers/Navbar';
+import Screen from '../components/containers/Screen';
 import { useState } from 'react';
-import { ReactComponent as Home } from '@UICOMPONENTS/svgs/Home.svg';
-import { ReactComponent as Settings } from '@UICOMPONENTS/svgs/Settings.svg';
+import { ReactComponent as Home } from '../components/svgs/Home.svg';
+import { ReactComponent as Settings } from '../components/svgs/Settings.svg';
 
 const Start = () => {
 	const HomeNavbarItem = {
