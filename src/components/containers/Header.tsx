a94aa6fb8ae32f8423d4fe785d394d4882c61d6c
damianamodeo <damianamodeotheocratic@gmail.com>
2023-02-05 13:33:12 +1100

    scriptBackup

--- src/components/containers/Header.tsx
+++ src/components/containers/Header.tsx
@@ -11,9 +11,9 @@ const Header = ({ headerLeft, title, headerRight, setPageToShow }: HeaderType) =
 	return (
 		<div>
 			<div className="flex place-items-center h-12 bg-lightGrey-300 dark:bg-darkGrey-700 border border-x-0 border-t-0 border-b-[.75px] border-y-lightGrey-500 dark:border-y-darkGrey-500">
-				<div className="text-secondary-500 dark:text-secondary-200">{headerLeft}</div>
+				<div className="text-secondary-500 dark:text-secondary-200 pl-2">{headerLeft}</div>
 				<div className="m-auto font-bold truncate">{title}</div>
-				<div className="mx-4 text-secondary-500 dark:text-secondary-200">{headerRight}</div>
+				<div className="mx-4 text-secondary-500 dark:text-secondary-200 pr-2">{headerRight}</div>
 			</div>
 		</div>
 	);
