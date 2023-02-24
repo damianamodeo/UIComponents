8c8b3e45ffb382dee9b696ce903b5a289de5c242
damianamodeo <damianamodeotheocratic@gmail.com>
2023-02-24 21:46:24 +1100

    commit on osana's macbook

--- src/icons/navbar/style.tsx
+++
@@ -1,7 +0,0 @@
-export const style = {
-	active: `stroke-primary fill-secondary-200 
-    dark:stroke-secondary-400  dark:fill-transparent`,
-
-	inactive: `stroke-black fill-transparent 
-    dark:stroke-white`,
-};
--- src/pages/buttons/buttons.tsx
+++
@@ -1,7 +0,0 @@
-import Page1 from './subPages/Page1';
-import Page2 from './subPages/Page2';
-import Page3 from './subPages/Page3';
-
-const ButtonsPage = [Page1, Page2, Page3];
-
-export default ButtonsPage;
